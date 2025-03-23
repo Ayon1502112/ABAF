ae46f543e44e8e78d88c80b0448c15ec10f05653
Ayon1502112 <152854568+Ayon1502112@users.noreply.github.com>
2025-03-24 01:00:13 +0600

    Contact.py

--- Contact.py
+++ Contact.py
@@ -6,9 +6,8 @@ st.title("Contact Information")
 
 # Hardcoded contact details in a dictionary
 data = {
-    "Name": ["John Doe", "Jane Smith", "Alice Johnson", "Bob Brown"],
-    "Phone Number": ["+1-123-456-7890", "+1-987-654-3210", "+1-555-666-7777", "+1-888-999-0000"],
-    "Email": ["john.doe@example.com", "jane.smith@example.com", "alice.johnson@example.com", "bob.brown@example.com"]
+    "নাম": ["অয়ন", "আসিফ", "অয়ন আম্মা", "আসিফ আম্মা","মাসুদ","হিমেল","উল্লাস"],
+    "মোবাইল নাম্বার": ["01678-863041// +880 1996-716061", "01835-272538 // +8801533090060", "01720578534 // +880 1948-636595", "01301496460","016-788-63651","017-147-12194","+880 1987-196753"]
 }
 
 # Convert dictionary to DataFrame
